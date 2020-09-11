7355f90b5e7466246fc2590ac4110b013b26f0b3
Noah Sweilem <nununoisy@gmail.com>
2020-09-10 23:36:12 -0700

    Add postgresql support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { Client: pgClient } = require('pg');
 
 const kojimaizer = require('./kojimaizer');
 
@@ -9,6 +10,8 @@ const SEC = 1000;
 //const DELAY = 10 * SEC;
 const DELAY = 30 * MIN;
 
+pgclient.connect();
+
 let guilds = [];
 
 client.once('ready', () => {
@@ -17,6 +20,7 @@ client.once('ready', () => {
 });
 
 setInterval(()=>{
+    console.log(`Sending messages to ${guilds.length} guilds`);
     guilds.forEach(guild => {
         if (guild.destChannel && guild.lastUsername) {
             guild.destChannel.send(kojimaizer(guild.lastUsername));
@@ -34,6 +38,7 @@ client.on('message', message=>{
             destChannel: null,
             lastUsername: null
         });
+        pgclient.query(`INSERT INTO guilds (gid, cid) VALUES ('${message.guild.id}','0');`);
     }
     let perms = new Discord.Permissions(message.channel.permissionsFor(message.guild.me).bitfield);
     if (message.mentions.has(message.guild.me) && message.member.permissions.has('MANAGE_GUILD', true)) {
@@ -41,6 +46,7 @@ client.on('message', message=>{
             console.log(`Set channel for ${guilds[guildObjIdx].id} to ${message.channel.id}`);
             guilds[guildObjIdx].destChannel = message.channel;
             message.channel.send('Hi Ad Min\n\nI Set The Current Channel To This One');
+            pgclient.query(`UPDATE guilds SET cid='${message.channel.id}' WHERE gid='${guilds[guildObjIdx].id}';`);
         } else {
             message.author.send(`Hi Ad Min\n\nI Can't Send Messages In #${message.channel.name} So I Did Not Change The Channel`);
         }
@@ -51,4 +57,25 @@ client.on('message', message=>{
     console.log(guilds[guildObjIdx].lastUsername);
 });
 
-client.login(process.env.BOTTOKEN);
+const pgclient = new pgClient({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+console.log('Querying postgresql db');
+pgclient.query('SELECT * FROM guilds;', (err, res)=>{
+    if (err) throw err;
+    console.log('Responded with row count', res.rows.length);
+    for (let row of res.rows) {
+        console.log(row);
+        guilds.push({
+            id: row.gid,
+            destChannel: row.cid,
+            lastUsername: null
+        });
+    }
+    console.log('Logging into discord');
+    client.login(process.env.BOTTOKEN);
+});

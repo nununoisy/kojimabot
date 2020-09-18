6eac7068dc0736ef1602102d5d3fe480df00ac2c
Noah Sweilem <nununoisy@gmail.com>
2020-09-18 13:59:40 -0700

    Add greet/farewell and channel filter

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ client.once('ready', () => {
     console.log('Login success');
 });
 
-let sentThisMinute = false;
-
 setInterval(()=>{
     console.log(`Sending messages to ${guilds.length} guilds`);
     guilds.forEach((guild, idx) => {
@@ -39,7 +37,43 @@ setInterval(()=>{
     });
 }, 15 * 60000);
 
-client.on('message', message=>{
+client.on('guildMemberAdd', member => {
+    let guild = member.guild;
+    let guildObjIdx = guilds.findIndex(guild=>guild.id===message.guild.id);
+    if (guildObjIdx === -1 || !guilds[guildObjIdx].entermessage) return;
+
+    if (guild.destChannel) {
+        if (typeof guild.destChannel === 'string') {
+            console.log(`Resolving channel ${guild.destChannel} for guild ${guild.id}`);
+            client.guilds.fetch(guild.id).then(guildobj=>{
+                guilds[idx].destChannel = guildobj.channels.resolve(guild.destChannel);
+                guild.destChannel.send(kojimaizer(member.user.username));
+            });
+        } else {
+            guild.destChannel.send(kojimaizer(member.user.username));
+        }
+    }
+});
+
+client.on('guildMemberRemove', member => {
+    let guild = member.guild;
+    let guildObjIdx = guilds.findIndex(guild=>guild.id===message.guild.id);
+    if (guildObjIdx === -1 || !guilds[guildObjIdx].leavemessage) return;
+
+    if (guild.destChannel) {
+        if (typeof guild.destChannel === 'string') {
+            console.log(`Resolving channel ${guild.destChannel} for guild ${guild.id}`);
+            client.guilds.fetch(guild.id).then(guildobj=>{
+                guilds[idx].destChannel = guildobj.channels.resolve(guild.destChannel);
+                guild.destChannel.send(kojimaizer(member.user.username).replace(/^Hi/, 'Bye'));
+            });
+        } else {
+            guild.destChannel.send(kojimaizer(member.user.username).replace(/^Hi/, 'Bye'));
+        }
+    }
+});
+
+client.on('message', message => {
     if (!message.guild) return;
     let guildObjIdx = guilds.findIndex(guild=>guild.id===message.guild.id);
     if (guildObjIdx === -1) {
@@ -47,23 +81,43 @@ client.on('message', message=>{
         guilds.push({
             id: message.guild.id,
             destChannel: null,
-            lastUsername: null
+            lastUsername: null,
+            entermessage: false,
+            leavemessage: false
         });
         pgclient.query(`INSERT INTO guilds (gid, cid) VALUES ('${message.guild.id}','0');`);
     }
-    let perms = new Discord.Permissions(message.channel.permissionsFor(message.guild.me).bitfield);
-    if (message.mentions.has(message.guild.me) && message.member.permissions.has('MANAGE_GUILD', true)) {
-        if (perms.has('SEND_MESSAGES')) {
-            console.log(`Set channel for ${guilds[guildObjIdx].id} to ${message.channel.id}`);
-            guilds[guildObjIdx].destChannel = message.channel;
-            message.channel.send('Hi Ad Min\n\nI Set The Current Channel To This One');
-            pgclient.query(`UPDATE guilds SET cid='${message.channel.id}' WHERE gid='${guilds[guildObjIdx].id}';`);
-        } else {
-            message.author.send(`Hi Ad Min\n\nI Can't Send Messages In #${message.channel.name} So I Did Not Change The Channel`);
+    if (message.mentions.has(message.guild.me) && message.member.permissions.has('MANAGE_GUILD', true) && !message.author.bot) {
+        if (message.content.indexOf('help') > -1) {
+            message.react('📤');
+            message.author.send('Hi Admin\n\nHere Are My Commands:\n `@HIDEO_KOJIMA help` - DM My Commands To You\n `@HIDEO_KOJIMA setchannel` - Set The Channel Where I Send Messages To The Channel Where You Entered The Command\n `@HIDEO_KOJIMA togglewelcome` - Enable Or Disable Welcome Messages\n `@HIDEO_KOJIMA togglefarewell` - Enable Or Disable Farewell Messages\n');
+        } else if (message.content.indexOf('setchannel') > -1) {
+            let perms = new Discord.Permissions(message.channel.permissionsFor(message.guild.me).bitfield);
+            if (perms.has('SEND_MESSAGES')) {
+                console.log(`Set channel for ${guilds[guildObjIdx].id} to ${message.channel.id}`);
+                guilds[guildObjIdx].destChannel = message.channel;
+                message.channel.send('Hi Ad Min\n\nI Set The Current Channel To This One');
+                pgclient.query(`UPDATE guilds SET cid='${message.channel.id}' WHERE gid='${guilds[guildObjIdx].id}';`);
+            } else {
+                message.author.send(`Hi Ad Min\n\nI Can't Send Messages In #${message.channel.name} So I Did Not Change The Channel`);
+            }
+        } else if (message.content.indexOf('togglewelcome') > -1) {
+            guilds[guildObjIdx].entermessage = !guilds[guildObjIdx].entermessage;
+            console.log(`${guilds[guildObjIdx].entermessage ? 'Enabled' : 'Disabled'} welcome for ${guilds[guildObjIdx].id}`);
+            message.channel.send(`Hi Ad Min\n\nI Will ${guilds[guildObjIdx].entermessage ? 'Now' : 'Not'} Greet People When They Join`);
+            pgclient.query(`UPDATE guilds SET entermsg='${guilds[guildObjIdx].entermessage}' WHERE gid='${guilds[guildObjIdx].id}';`);
+        } else if (message.content.indexOf('togglefarewell') > -1) {
+            guilds[guildObjIdx].leavemessage = !guilds[guildObjIdx].leavemessage;
+            console.log(`${guilds[guildObjIdx].leavemessage ? 'Enabled' : 'Disabled'} farewell for ${guilds[guildObjIdx].id}`);
+            message.channel.send(`Hi Ad Min\n\nI Will ${guilds[guildObjIdx].leavemessage ? 'Now' : 'Not'} Greet People When They Leave`);
+            pgclient.query(`UPDATE guilds SET leavemsg='${guilds[guildObjIdx].leavemessage}' WHERE gid='${guilds[guildObjIdx].id}';`);
         }
         return;
     }
     if (message.member.id === message.guild.me.id) return;
+    let cidtest = guilds[guildObjIdx].destChannel;
+    if (typeof cidtest !== 'string') cidtest = cidtest.id;
+    if (message.channel.id !== cidtest) return;
     guilds[guildObjIdx].lastUsername = message.author.username;
     console.log(guilds[guildObjIdx].lastUsername);
 });
@@ -77,7 +131,9 @@ pgclient.query('SELECT * FROM guilds;', (err, res)=>{
         guilds.push({
             id: row.gid,
             destChannel: row.cid,
-            lastUsername: null
+            lastUsername: null,
+            entermessage: row.entermsg,
+            leavemessage: row.leavemsg
         });
     }
     console.log('Logging into discord');

dcccce1b5d2899525df8e0a474e567640bb730ab
Noah Sweilem <nununoisy@gmail.com>
2020-11-19 12:52:15 -0700

    Add even more error debug info

--- espeak-ng/index.js
+++ espeak-ng/index.js
@@ -1,10 +1,11 @@
 const worker = require('./espeakng.worker');
 
 module.exports = async word => {
+    console.log('Katakanifier start...')
     const espeakng = await Promise.resolve(worker);
-
+    console.log('Resolved espeak-ng...')
     const res = espeakng.synthesize_ipa(word);
-
+    console.log('Espeak-ng finished...')
     if (res.code === 0) return res.ipa;
     console.log('ESPEAK-NG ERROR');
     throw new Error(`espeak-ng failed with code ${res.code}`);
